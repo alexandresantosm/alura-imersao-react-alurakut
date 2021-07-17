88ec0a957fbe5f94fc26ebd56fa951abc143f5f8
Alexandre Santos <amenezes031@gmail.com>
2021-07-17 15:52:40 -0300

    aula01: inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,72 @@
-import styled from 'styled-components'
+import { Box } from '../src/components/Box'
+import { MainGrid } from '../src/components/MainGrid'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        alt={`Foto do usuário no github ${props.githubUser}`}
+        style={{ borderRadius: '8px' }}
+      />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'alexandresantosm'
+  const favoritePeople = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">Bem-vindo(a), Alexandre</h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: 'profileRelationsArea' }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Meus amigos ({favoritePeople.length})
+            </h2>
+            <ul>
+              {favoritePeople.map((item, index) => (
+                <li>
+                  <a href={`/users/${item}`} key={index}>
+                    <img
+                      src={`https://github.com/${item}.png`}
+                      alt={`Foto do usuário no github ${item}`}
+                    />
+                    <span>{item}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
